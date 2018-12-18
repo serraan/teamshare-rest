1aae8b001a7e01f733b3a23a2f1eff7e2b83b132
Gabriele Rizzo <gabriele.rizzo@linksmt.it>
2018-12-18 15:47:19 +0100

    elimina

--- TeamShare.zip_expanded/teamshare-dao/src/main/java/it/linksmt/teamshare/dao/repositories/ReactionRepository.java
+++
@@ -1,9 +0,0 @@
-package it.linksmt.teamshare.dao.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import it.linksmt.teamshare.dao.entities.Reaction;
-
-public interface ReactionRepository extends CrudRepository<Reaction, Integer> {
-
-}
