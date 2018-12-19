98d8f4f7f3e8d3f0ba38f73a8668ada345f31eed
Gabriele Rizzo <gabriele.rizzo@linksmt.it>
2018-12-19 16:53:09 +0100

    Eliminato progetto

--- teamshare-build/teamshare-dao/src/main/java/it/linksmt/teamshare/dao/repositories/ActivitySprintRepository.java
+++
@@ -1,9 +0,0 @@
-package it.linksmt.teamshare.dao.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import it.linksmt.teamshare.dao.entities.ActivitySprint;
-
-public interface ActivitySprintRepository extends CrudRepository<ActivitySprint, Integer> {
-
-}
--- teamshare-build/teamshare-dao/src/main/java/it/linksmt/teamshare/dao/repositories/ReleaseRepository.java
+++
@@ -1,9 +0,0 @@
-package it.linksmt.teamshare.dao.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import it.linksmt.teamshare.dao.entities.Release;
-
-public interface ReleaseRepository extends CrudRepository<Release, Integer> {
-
-}
